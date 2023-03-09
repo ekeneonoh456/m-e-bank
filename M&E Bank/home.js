de73ac0ef3871f2c5931733e4923d2e483116f75
ekeneonoh456 <110058917+ekeneonoh456@users.noreply.github.com>
2023-03-09 16:53:04 -0500

    Changed home page to show deposit/withdraw. Does not work yet

--- M&E Bank/home.js
+++ M&E Bank/home.js
@@ -1,23 +1,9 @@
-// Check if there is already a vlaue in local storage
-if (!localStorage.getItem("counter")) {
-	// If not, set the counter to 0 in local storage
-	localStorage.setItem("counter", 0);
-}
+const form = document.querySelector("form");
 
-function count() {
-	// Retrieve counter value from local storage
-	let counter = localStorage.getItem("counter");
-
-	// update counter
-	counter++;
-	document.querySelector("h1").innerHTML = counter;
-
-	// Store counter in local storage
-	localStorage.setItem("counter", counter);
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-	// Set heading to the current value inside local storage
-	document.querySelector("h1").innerHTML = localStorage.getItem("counter");
-	document.querySelector("button").onclick = count;
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const fd = new FormData(form);
+	for (item of fd) {
+		console.log(item);
+	}
 });
